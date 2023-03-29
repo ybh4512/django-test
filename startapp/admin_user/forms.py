e30681b75c3db5da83e2b7ec3f080652692f96ee
ybh4512 <ybh4512@evrstudio.com>
2023-03-29 12:07:43 +0900

    Form에 instance를 넘기도록 수정

--- startapp/admin_user/forms.py
+++ startapp/admin_user/forms.py
@@ -1,7 +1,10 @@
 from django import forms
+from .models import AdminUser
 
-class AdminUserForm(forms.Form):
+class AdminUserForm(forms.ModelForm):
     name = forms.CharField(max_length=100)
     email = forms.CharField(max_length=300)
-    created_at = forms.DateTimeField()
-    updated_at = forms.DateTimeField()
+
+    class Meta:
+        model = AdminUser
+        fields = '__all__'
--- startapp/admin_user/views.py
+++ startapp/admin_user/views.py
@@ -33,8 +33,8 @@ def new(request):
 
 def edit(request, admin_user_id):
     admin_user = AdminUser.objects.get(pk=admin_user_id)
-    f = AdminUserForm({'name':admin_user.name, 'email':admin_user.email, 'created_at':admin_user.created_at, 'updated_at':admin_user.updated_at})
-    # f = AdminUserForm(admin_user)
+    # f = AdminUserForm({'name':admin_user.name, 'email':admin_user.email, 'created_at':admin_user.created_at, 'updated_at':admin_user.updated_at})
+    f = AdminUserForm(instance=admin_user)
     return render(request, "edit.html", {'form':f, "admin_user_id":admin_user_id})
 
 
@@ -49,8 +49,8 @@ def create(request):
     new_user = AdminUser()
     new_user.name = form.cleaned_data['name']
     new_user.email = form.cleaned_data['email']
-    new_user.created_at = form.cleaned_data['created_at']
-    new_user.updated_at = form.cleaned_data['updated_at']
+    new_user.created_at = datetime.datetime.now()
+    new_user.updated_at = datetime.datetime.now()
     new_user.save()
 
     return redirect("/admin_user/{}/".format(new_user.id))
@@ -67,8 +67,7 @@ def update(request, admin_user_id):
     user = AdminUser.objects.get(pk=admin_user_id)
     user.name = form.cleaned_data['name']
     user.email = form.cleaned_data['email']
-    user.created_at = form.cleaned_data['created_at']
-    user.updated_at = form.cleaned_data['updated_at']
+    user.updated_at = datetime.datetime.now()
     user.save()
 
     return redirect("/admin_user/{}/".format(admin_user_id))
